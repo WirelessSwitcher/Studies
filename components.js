4a1c54ccb0c7e4a3b103ffc8bfe45e5d44617b0c
WirelessSwitcher Thatzzit <jose.vitorandrade@hotmail.com>
2021-05-21 07:20:16 +0200

    7th commit

--- components.js
+++ components.js
@@ -2,5 +2,7 @@ var canvas = document.getElementById("projectArea");
 var ctx = canvas.getContext("2d");
 
 function drawGrid(c, r, sc, sr){
-
+    ctx.moveTo(300,30s0);
+    lineTo(200,100);
+    ctx.stroke();
 };
