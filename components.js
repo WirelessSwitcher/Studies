ada9ba036fa6a2eb14581aa98d283df86079725f
WirelessSwitcher <jose.vitorandrade@hotmail.com>
2021-06-07 14:33:57 +0200

    Change projectArea background colour to white and removed function from component.js

--- components.js
+++ components.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("projectArea");
 var ctx = canvas.getContext("2d");
 
 //function drawGrid(c, r, sc, sr){
-    ctx.moveTo(300,30s0);
+    ctx.moveTo(300,300);
     lineTo(200,100);
     ctx.stroke();
 //};
