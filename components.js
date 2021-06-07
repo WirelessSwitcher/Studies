bd860de0a2ef5c6944dfa3134cbb94e437f1e4cb
WirelessSwitcher <jose.vitorandrade@hotmail.com>
2021-06-07 14:29:10 +0200

    Testing git

--- components.js
+++ components.js
@@ -1,8 +1,8 @@
 var canvas = document.getElementById("projectArea");
 var ctx = canvas.getContext("2d");
 
-function drawGrid(c, r, sc, sr){
+//function drawGrid(c, r, sc, sr){
     ctx.moveTo(300,30s0);
     lineTo(200,100);
     ctx.stroke();
-};
+//};
