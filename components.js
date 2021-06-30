a352975f95e52d21858e65ed558a5f948f818b73
WirelessSwitcher <jose.vitorandrade@hotmail.com>
2021-06-30 11:33:18 +0200

    git push

--- components.js
+++ components.js
@@ -1,20 +1,8 @@
-
-function resize(){    
-    $("#canvas").outerHeight($(window).height()-$("#canvas").offset().top- Math.abs($("#canvas").outerHeight(true) - $("#canvas").outerHeight()));
-  }
-  $(document).ready(function(){
-    resize();
-    $(window).on("resize", function(){                      
-        resize();
-    });
-  });
-
 var canvas = document.getElementById("projectArea");
 var ctx = canvas.getContext("2d");
-
 
 //function drawGrid(c, r, sc, sr){
     ctx.moveTo(300,300);
     ctx.lineTo(200,100);
     ctx.stroke();
-//};
+//};s
