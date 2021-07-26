01a56c3168bc9be553f75b8ee213958514871f70
José Vitor Andrade <WirelessSwitcher@users.noreply.github.com>
2021-07-26 08:57:47 +0200

    Update 26062021
    Added comments to improve readability and debounce function for window resizing.

--- design.js
+++ design.js
@@ -1,19 +1,22 @@
-const canvas = document.getElementById("projectArea");
-const ctx = canvas.getContext("2d");
-const ratio = 4/3;
+// SCADA Drawing Area and Elements
+const canvas = document.getElementById("projectArea");												// Get the project area properties
+const ctx = canvas.getContext("2d");																// Defines a 2D plane to draw on it
 
-const delay = 1000;																					// Delay in ms
+// Settings
+const delay = 1000;																					// Delay in ms when window is resized
+const ratio = 4/3;																					// Project area screen proportions (width by height)
 
 var count = 0;
 var resizeFlag = 0;
 
-//window.onload = debounceResize(delay);
-window.onresize = debounceResize;
+//window.onload = drawCanvas;
+window.onresize = new debounceResizeEvent(delay);
 
 function drawCanvas(){
+	console.log ("Canvas is being  drawn");
+
 	var winW = window.innerWidth;
 	var winH = window.innerHeight;
-
 	var screenMode = null;
 	var canvasMode = null;
 
@@ -27,47 +30,47 @@ function drawCanvas(){
 	var canH = 0;
 
 	switch(winW > winH){
-		case true:
-			canvasMode = 1;																			// Canvas will be landscape, here height is the constraint
+			case true:
+				canvasMode = 1;																		// Canvas will be landscape, here height is the constraint
+				
+				break;
+			case false:
+				canvasMode = 0;																		// Canvas will be portrait, here width is the constraint
+				break;
+			default:
+				canvasMode = null																	// Canvas will adapt to screen
+		}
+
+		if(resizeFlag == 1){
+
+			if(winW > winH){
+				console.log("Landscape mode");
+			}
+			else if (winW < winH){
+				console.log("Portrait mode");
+			}
+			else {
+				console.log("Square screen");
+			}
+
+			const redrawCount = count++;
 			
-			break;
-		case false:
-			canvasMode = 0;																			// Canvas will be portrait, here width is the constraint
-			break;
-		default:
-			canvasMode = null																		// Canvas will adapt to screen
-	}
+			console.log("Proto width 1 is: " + protoW1
+			+ "\n" + "Proto height 1 is: " + protoH1
+			+ "\n" + "Proto width 2 is: " + protoW2
+			+ "\n" + "Proto height 2 is: " + protoH2
+			//+ "\n" + "Orientation is: " + orientation
+			+ "\n" + "Times loadded: " + count
+			);
 
-	if(resizeFlag == 1){
-
-		if(winW > winH){
-			console.log("Landscape mode");
+			// Reset resize flag when done
+			resizeFlag = 0;
 		}
-		else if (winW < winH){
-			console.log("Portrait mode");
-		}
-		else {
-			console.log("Square screen");
-		}
-
-		const redrawCount = count++;
-		
-		console.log("Proto width 1 is: " + protoW1
-		+ "\n" + "Proto height 1 is: " + protoH1
-		+ "\n" + "Proto width 2 is: " + protoW2
-		+ "\n" + "Proto height 2 is: " + protoH2
-		//+ "\n" + "Orientation is: " + orientation
-		+ "\n" + "Times loadded: " + count
-		);
-
-		// Reset resize flag when done
-		resizeFlag = 0;
-	}
 }
 
-function debounceResize(){
-	var delayTime = parseInt(delay);
-	console.log(delayTime);
-
+function debounceResizeEvent(SP){
+    let mySP = parseInt(SP);
+	console.log("Delay is: " + mySP + "ms");
 	resizeFlag = 1;
+	setTimeout(drawCanvas, mySP);
 }
