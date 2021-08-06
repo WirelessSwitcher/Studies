81c1d4aeb2a5bb7f0c5417fc5cd449ed8da606e8
WirelessSwitcher <jose.vitorandrade@hotmail.com>
2021-08-06 11:38:28 +0200

    server

--- global.js
+++ global.js
@@ -11,11 +11,14 @@
 var globalVar1 = "Test3";
 
 var http = require("http");
+var fs = require("fs");
 
 function onRequest(request, response){
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world!");
-    response.end();
+    fs.readFile("index.html", function(err, data){
+        response.writeHead(200, {"Contetn-Type": "text/html"});
+        response.write(data);
+        return response.end();
+    });
 }
 
 http.createServer(onRequest).listen(3000);
