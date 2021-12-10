835c7f561827a7857ea87ff9d0fb3e06523a87f4
WirelessSwitcher <jose.vitorandrade@hotmail.com>
2021-12-10 16:11:14 +0100

    Major changes in structure

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 /*
-    Creation date: 01/06/2021
+    Creation date: 10/12/2021
     Author: Andrade, J.V.
 
     This shall contain the server functions and global variables.
@@ -8,25 +8,16 @@
     Comment: added header and document's
 */
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
-var serveStatic = require('serve-static')
 
-var server = http.createServer(function onRequest (req, res){
-    serve(req, res, finalhandler(req,res));
+app.listen(3000, () => {
+    console.log("Application started and Listening on port 3000");
 });
 
-server.listen(3000);
+// serve your css as static
+app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-        res.send('Hello World!');
-    }
-);
-  
-app.listen(port, () => {
-        console.log(`Example app listening at http://localhost:${port}`);
-    }
-);
-
-app.use(express.static('public'));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
