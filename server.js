93b2bd5402589cf359e24dbf1906757fc1065105
WirelessSwitcher <jose.vitorandrade@hotmail.com>
2021-12-10 13:23:43 +0100

    Removed the server.js since it was redundant

--- server.js
+++
@@ -1,6 +0,0 @@
-// I don't know what to do here yet
-
-// Maybe the player object should be here
-// Maybe the player 2 object should be here
-// Maybe the ball object should be here...
-// This WPA architecture is weirdd
